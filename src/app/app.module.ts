76dd7038a016f0b3fa269f83cf4943860b442f03
Victor <utrera1695@gmail.com>
2020-07-12 11:16:28 -0400

    adding firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDzWItyZUWX57PLyksu-hgodLm4gX4pD5Q',
+    authDomain: 'ionic6-angular-cleanarch.firebaseapp.com',
+    databaseURL: 'https://ionic6-angular-cleanarch.firebaseio.com',
+    projectId: 'ionic6-angular-cleanarch',
+    storageBucket: 'ionic6-angular-cleanarch.appspot.com',
+    messagingSenderId: '695084292151',
+    appId: '1:695084292151:web:f299bbbba4e990c88347b2',
+    measurementId: 'G-SCS6GTBH2X',
+  },
 };
 
 /*
